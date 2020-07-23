6643ac8027f2369791c1c83e785212142aefa262
elgong <1552460315@qq.com>
2020-07-23 23:20:27 +0800

    --new=添加解析配置

--- config/config.go
+++ config/config.go
@@ -1,28 +1,58 @@
-// @Title  全局配置项
-// @Author  elgong 2020.7.23
-// @Update  elgong 2020.7.23
-package main
+package config
 
 import (
 	"fmt"
 	"io/ioutil"
+	"log"
 
 	"gopkg.in/yaml.v2"
 )
 
-type Config struct {
-	ip      string `yaml:"IP"`
-	service string `yaml:"name"`
-	//service struct {
-	//	ip string `yaml: "ip"`
-	//}
+// DefalutGlobalConfig 全局使用的配置参数
+var DefalutGlobalConfig *Config
+
+func init() {
+	err := Load("./config/config.yaml", DefalutGlobalConfig)
+
+	if err != nil {
+		panic("参数解析异常")
+	}
+
+	fmt.Println("默认参数加载成功")
 }
 
-func main() {
-	content, _ := ioutil.ReadFile("./elgongRPC.yaml")
-	env := &Config{}
-	err := yaml.Unmarshal(content, env)
+type Config struct {
+	Name string `yaml:"name"`
+	Conn struct {
+		MaxConn string `yaml:"maxConn"`
+		MinConn string `yaml:"minConn"`
+	}
 
-	fmt.Println(err, env)
+	Codec struct {
+		Codec string `yaml:"codec-method"`
+	}
 
+	Server struct {
+		Servicename   string `yaml:"servicename"`
+		Servicemethod string `yaml:"servicemethod"`
+		Ip            string `yaml:"ip"`
+	}
+}
+
+// Load 解析配置参数
+func Load(path string, config *Config) error {
+	conf := new(Config)
+	yamlFile, err := ioutil.ReadFile(path)
+
+	if err != nil {
+		log.Println("yamlFile.Get err:", err)
+		return err
+	}
+	err = yaml.Unmarshal(yamlFile, &conf)
+	if err != nil {
+		log.Fatal("Unmarshal:", err)
+		return err
+	}
+	config = conf
+	return err
 }
